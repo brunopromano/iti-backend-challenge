34d0eb285d6420c6e60d6835db5f46813251d685
brunopromano <brunopromano@gmail.com>
2021-05-28 15:00:55 -0300

    Adds Swagger for API documentation and global.json

--- src/Iti.Challenge.RestApi/Startup.cs
+++ src/Iti.Challenge.RestApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Iti.Challenge.RestApi
 {
@@ -25,7 +26,26 @@ namespace Iti.Challenge.RestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddControllers()
+                    .AddNewtonsoftJson();
+
+            services.AddSwaggerGenNewtonsoftSupport();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerGeneratorOptions.IgnoreObsoleteActions = true;
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Iti Challenge Rest API",
+                    Description = "Rest API to check if a password is valid for Iti Backend Challenge",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Bruno Romano",
+                        Email = "brunopromano@gmail.com",
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +55,13 @@ namespace Iti.Challenge.RestApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API PainelContabil v1");
+            });
 
             app.UseHttpsRedirection();
 
